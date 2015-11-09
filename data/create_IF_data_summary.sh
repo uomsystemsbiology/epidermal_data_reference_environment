de10d91e9ebc135b449c934dc462f533663a2726
Daniel Hurley <daniel.hurley@unimelb.edu.au>
2015-11-09 17:20:43 +1100

    Fixed wrong filenames in create_IF_data_summary.sh

--- data/create_IF_data_summary.sh
+++ data/create_IF_data_summary.sh
@@ -9,11 +9,11 @@ log=/vagrant/temp/run_experiments.log
 
 echo Copying the compiled code into the project directory | tee -a $log
 cp /home/sbl/epidermal_data/compiled/create_IF_data_summary  /home/sbl/epidermal_data/code/create_IF_data_summary
-cp /home/sbl/epidermal_data/compiled/create_IF_data_summary.sh  /home/sbl/epidermal_data/code/create_IF_data_summary.sh  
+cp /home/sbl/epidermal_data/compiled/create_IF_data_summary.sh  /home/sbl/epidermal_data/code/run_create_IF_data_summary.sh  
 
 echo Executing create_IF_data_summary | tee -a $log
 cd /home/sbl/epidermal_data/code
-./create_IF_data_summary.sh /usr/local/MATLAB/MATLAB_Runtime/v85
+./run_create_IF_data_summary.sh /usr/local/MATLAB/MATLAB_Runtime/v85
 
 echo Completed analysis | tee -a $log
 $SHELL
