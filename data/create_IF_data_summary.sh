8ab14bea3e11a2ce35260cfd786eb7a18c8249db
Daniel Hurley <daniel.hurley@unimelb.edu.au>
2015-11-10 19:08:32 +1100

    Fixed cp bug in create_IF_data_summary.sh

--- data/create_IF_data_summary.sh
+++ data/create_IF_data_summary.sh
@@ -9,7 +9,7 @@ log=/vagrant/temp/run_experiments.log
 
 echo Copying the compiled code into the project directory | tee -a $log
 cp /home/sbl/epidermal_data/compiled/create_IF_data_summary  /home/sbl/epidermal_data/code/create_IF_data_summary
-cp /home/sbl/epidermal_data/compiled/create_IF_data_summary.sh  /home/sbl/epidermal_data/code/run_create_IF_data_summary.sh  
+cp /home/sbl/epidermal_data/compiled/run_create_IF_data_summary.sh  /home/sbl/epidermal_data/code/run_create_IF_data_summary.sh  
 
 echo Executing create_IF_data_summary | tee -a $log
 cd /home/sbl/epidermal_data/code
